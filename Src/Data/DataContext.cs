a5ed69996b156eb80ca225df004c955b96ab77a2
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-03-14 21:24:19 -0300

    feat: create database context with posts list

---
+++ Src/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using image_uploader_dotnet_api.Src.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace image_uploader_dotnet_api.Src.Data;
+
+class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Post> Posts { get; set; }
+}
