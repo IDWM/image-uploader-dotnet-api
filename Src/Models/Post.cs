755685656cdc4a1bd688f01c0272b9ce0f0cb180
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-03-16 13:58:22 -0300

    feat: update post model with new properties

--- Src/Models/Post.cs
+++ Src/Models/Post.cs
@@ -3,5 +3,8 @@ namespace image_uploader_dotnet_api.Src.Models;
 public class Post
 {
     public long Id { get; set; }
-    public required string Name { get; set; }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required string Url { get; set; }
+    public required string PublicId { get; set; }
 }
